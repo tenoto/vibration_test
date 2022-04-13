13c5210397fa6bab81855a8bf4e9ceb07ebc3677
Teruaki Enoto <teruaki.enoto@riken.jp>
2022-04-13 17:20:25 +0900

    send

--- script/plot_random_vibration.py
+++ script/plot_random_vibration.py
@@ -16,7 +16,27 @@ df_slingshot = pd.read_excel('data/Random Vibration Enviroment.xlsx',
 	sheet_name='SlingShot',engine='openpyxl',skiprows=[0,1,3])
 print(df_slingshot)
 
-plt.style.use('script/default.mplstyle')
+df_vegac_top = pd.read_excel('data/Random Vibration Enviroment.xlsx',
+	sheet_name='VegaC_top',engine='openpyxl',skiprows=[0,1,3])
+print(df_vegac_top)
+
+df_vegac_main = pd.read_excel('data/Random Vibration Enviroment.xlsx',
+	sheet_name='VegaC_main',engine='openpyxl',skiprows=[0,1,3])
+print(df_vegac_main)
+
+df_vegac_hexagon = pd.read_excel('data/Random Vibration Enviroment.xlsx',
+	sheet_name='VegaC_hexagon',engine='openpyxl',skiprows=[0,1,3])
+print(df_vegac_hexagon)
+
+df_gevs_component = pd.read_excel('data/Random Vibration Enviroment.xlsx',
+	sheet_name='GEVS_component',engine='openpyxl',skiprows=[0,1,3])
+print(df_gevs_component)
+
+df_gevs_component_elv = pd.read_excel('data/Random Vibration Enviroment.xlsx',
+	sheet_name='GEVS_componentELV',engine='openpyxl',skiprows=[0,1,3])
+print(df_gevs_component_elv)
+
+#plt.style.use('script/default.mplstyle')
 
 fig = plt.figure(figsize=(12,9))
 fig.patch.set_alpha(0.0)
@@ -30,12 +50,23 @@ ax1.grid(axis='both',which='minor', linestyle='--')
 ax1.tick_params(axis="both", which='major', direction='in', length=5)
 ax1.tick_params(axis="both", which='minor', direction='in', length=3)
 
-ax1.plot(df_spacex['Frequency'],df_spacex['MPE PSD'],'r-',
-	linewidth=2,label='SpaceX')
-ax1.plot(df_cygnus['Frequency'],df_cygnus['PSD'],'b-',
+ax1.plot(df_spacex['Frequency'],df_spacex['MPE PSD'],'c.-',
+	linewidth=4,label='SpaceX')
+ax1.plot(df_cygnus['Frequency'],df_cygnus['PSD'],'b--',
 	linewidth=2,label='Cygnus')
-ax1.plot(df_slingshot['Frequency'],df_slingshot['MPE PSD'],'k-',
+ax1.plot(df_slingshot['Frequency'],df_slingshot['MPE PSD'],'k--',
 	linewidth=2,label='SlingShot')
+ax1.plot(df_vegac_top['Frequency'],df_vegac_top['MPE PSD'],'g--',
+	linewidth=2,label='Vega-C top')
+ax1.plot(df_vegac_main['Frequency'],df_vegac_main['MPE PSD'],'c--',
+	linewidth=2,label='Vega-C main')
+ax1.plot(df_vegac_hexagon['Frequency'],df_vegac_hexagon['MPE PSD'],'m--',
+	linewidth=2,label='Vega-C hexagon')
+ax1.plot(df_gevs_component['Frequency'],df_gevs_component['MPE PSD'],'b-.',
+	linewidth=4,label='GEVS component (min)')
+ax1.plot(df_gevs_component_elv['Frequency'],df_gevs_component_elv['MPE PSD']/1.995,'r-',
+	linewidth=4,label='GEVS component (ELV) acceptance - 3dB')
+
 
 ax1.set_xlim(20,2000)
 #ax1.set_ylim(1e-4,1e+6)
